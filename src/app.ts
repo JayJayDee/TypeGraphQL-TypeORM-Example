ad644adb4ddbb8938e5f69b8cffb8651eb10d195
JayJayDee <jindongp@gmail.com>
2019-09-10 14:57:15 +0900

    first working example.

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,42 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server';
 import { ObjectType, Field, ID, Resolver, Arg, Query, buildSchema } from 'type-graphql';
+import { createConnection, Entity, PrimaryGeneratedColumn, Column, getConnection } from 'typeorm';
 
 @ObjectType()
-class ImagePayload {
-  @Field()
-  imageUrl: string;
-
-  @Field()
-  thumbUrl: string;
-}
-
-@ObjectType()
+@Entity({ name: 'message' })
 class Message {
   @Field(type => ID)
-  id: string;
+  @PrimaryGeneratedColumn()
+  id: number;
 
   @Field({ nullable: true })
+  @Column({ nullable: true, length: 30 })
   text?: string;
-
-  @Field({ nullable: true })
-  image?: ImagePayload;
 }
 
 @Resolver(of => Message)
 class MessageResolver {
 
   @Query(returns => Message)
-  async message(@Arg('id') id: string): Promise<Message> {
-    const msg = new Message();
+  async message(@Arg('id') id: number): Promise<Message | undefined> {
+    const msg = await getConnection().getRepository(Message).findOne(id);
     return msg;
   }
 }
 
 (async () => {
+  await createConnection({
+    name: 'default',
+    type: 'mysql',
+    host: '127.0.0.1',
+    username: 'root',
+    password: 'hands',
+    database: 'typeorm_test',
+    entities: [ Message ],
+    synchronize: true
+  });
+
   const schema = await buildSchema({
     resolvers: [ MessageResolver ]
   });
